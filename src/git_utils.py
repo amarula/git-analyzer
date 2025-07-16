amarula/git-analyzer

commit 5d136d1b8bfc28dcb8eaa9784aab0d3d12bcc42b

src: git_utils: Add git.openembedded.org as special prefix
This repo is not compatible with github and gitlab to look for
a commit. We need to add a way to fix it on the way to calculate
the commit url.

Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

src/git_utils.py
@@ -34,10 +34,18 @@ def generate_commit_hyperlink(repo_path, base_web_url, commit_hash_prefix):
 
         commit_full_hash = commit.hexsha
         commit_message = commit.summary
+        special_cases_prefixes = [
+            "https://git.kernel.org",
+            "https://git.openembedded.org"
+        ]
 
-        if base_web_url.startswith("https://git.kernel.org"):
-            commit_url = f"{base_web_url}/commit/?id={commit_full_hash}"
-        else:
+        commit_url = None
+        for prefix in special_cases_prefixes:
+            if base_web_url.startswith(prefix):
+                commit_url = f"{base_web_url}/commit/?id={commit_full_hash}"
+                break
+
+        if commit_url is None
             if base_web_url.endswith(".git"):
                 base_web_url = base_web_url[:-4]
             commit_url = f"{base_web_url}/commit/{commit_full_hash}"

commit 16c88ebb7e3039cc68d673ef9a90c1e9c5a226d0

src: git_utils: Fix synthax error
Fixes: 5d136d1b8bfc ("src: git_utils: Add git.openembedded.org as special prefix")
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

src/git_utils.py
@@ -45,7 +45,7 @@ def generate_commit_hyperlink(repo_path, base_web_url, commit_hash_prefix):
                 commit_url = f"{base_web_url}/commit/?id={commit_full_hash}"
                 break
 
-        if commit_url is None
+        if commit_url is None:
             if base_web_url.endswith(".git"):
                 base_web_url = base_web_url[:-4]
             commit_url = f"{base_web_url}/commit/{commit_full_hash}"