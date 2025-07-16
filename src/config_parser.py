amarula/git-analyzer

commit be7e2d2202e1af0d61b61f42d71e41d618bb4f02

Add openai key parameter and option of the config file
Allow to get the openai key from command line or file ini.

Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

src/config_parser.py
@@ -25,6 +25,7 @@ def load_config_from_ini(file_path: str) -> dict | None:
             )
             git_config["months_back"] = config["GitConfig"].getint("months_back", None)
             git_config["deploy_dir"] = config["GitConfig"].get("deploy_dir", None)
+            git_config["openai_apikey"] = config["OpenAi"].get("openai_apikey", None)
         return git_config
     except Exception as e:
         print(f"Error reading INI file {file_path}: {e}")

main.py
@@ -54,6 +54,13 @@ def main():
         type=str,
         default="deploy",
     )
+    parser.add_argument(
+        "-k",
+        "--openai-key",
+        help="Pass the openai key for create commit summary by Author and repo.",
+        type=str,
+        default=None,
+    )
 
     args = parser.parse_args()
 
@@ -62,6 +69,7 @@ def main():
     months_back = None
     save_file_name = None
     deploy_dir = None
+    openai_key = None
 
     config_loaded_successfully = False
 
@@ -73,6 +81,8 @@ def main():
             company_identifier = config_data.get("company_identifier", "")
             months_back = config_data.get("months_back", None)
             deploy_dir = config_data.get("deploy_dir", None)
+            openai_apikey = config_data.get("openai_apikey", None)
+
             config_loaded_successfully = True
             print(f"Configuration loaded from {args.config_file}.")
         else:
@@ -93,6 +103,8 @@ def main():
             months_back = args.months_back
         if args.deploy_dir:
             deploy_dir = args.deploy_dir
+        if args.openai_key:
+            openai_key = args.openai_key
 
     if args.deploy_dir:
         deploy_dir = args.deploy_dir

commit 1bd035d01007b02c1569a49e0dfb9d13d8940e8b

src: config_parser: Check if OpenAi section exists before get the openai key
Fixes: be7e2d2202e1 ("Add openai key parameter and option of the config file")
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

src/config_parser.py
@@ -25,6 +25,7 @@ def load_config_from_ini(file_path: str) -> dict | None:
             )
             git_config["months_back"] = config["GitConfig"].getint("months_back", None)
             git_config["deploy_dir"] = config["GitConfig"].get("deploy_dir", None)
+        if "OpenAi" in config:
             git_config["openai_apikey"] = config["OpenAi"].get("openai_apikey", None)
         return git_config
     except Exception as e: